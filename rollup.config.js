de79495ffec483f324051d96f459a8dd41f8d38f
zhik <zhikhe@gmail.com>
2021-01-04 18:14:17 -0500

    rollup fixes

--- rollup.config.js
+++ rollup.config.js
@@ -23,7 +23,7 @@ export default {
       // we'll extract any component CSS out into
       // a separate file — better for performance
       css: css => {
-        css.write('public/build/bundle.css')
+        css.write('bundle.css')
       }
     }),
     // If you have external dependencies installed from
